3a5338d6b32246461e7e6eb0e00f07c8af777625
Sebastian Ortega <sebastian.ortega.fernandezz@gmail.com>
2023-08-06 20:48:00 -0400

    login materialUI

--- src/auth/pages/Login.jsx
+++ src/auth/pages/Login.jsx
@@ -1,4 +1,5 @@
-import {Box, Grid, Typography} from "@mui/material";
+import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Google } from '@mui/icons-material';
 
 export const Login = () => {
   return (
@@ -24,9 +25,48 @@ export const Login = () => {
                 >
                     Login
                 </Typography>
+                <form>
+                    <Grid
+                        container
+                    >
+                        <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                            <TextField
+                                label="Correo"
+                                placeholder="correo@gmail.com"
+                                type="email"
+                                fullWidth
+                            />
+                        </Grid>
+
+                        <Grid item xs={ 12 } sx={{ mt: 2 }}>
+                            <TextField
+                                label="Password"
+                                type="password"
+                                placeholder="********"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+                            <Grid item xs={ 12 } sm={ 6 }>
+                                <Button variant='contained' fullWidth>
+                                    <Typography >
+                                        Login
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                            <Grid item xs={ 12 } sm={ 6 } >
+                                <Button variant='contained' fullWidth>
+                                    <Google />
+                                    <Typography sx={{ ml: 1}}>
+                                        Google
+                                    </Typography>
+                                </Button>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </form>
             </Grid>
         </Grid>
-
       </>
   )
 }
